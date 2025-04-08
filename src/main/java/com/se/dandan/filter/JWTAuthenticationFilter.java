497c44b26352b3fd66a4deac6b98ec46d1f944ff
Etwashoeren <sungmin0609@nate.com>
2025-04-08 23:52:54 +0900

    #3 feat: JWT 검증 필터 작성

--- src/main/java/com/se/dandan/filter/JWTAuthenticationFilter.java
+++ src/main/java/com/se/dandan/filter/JWTAuthenticationFilter.java
@@ -7,6 +7,8 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
@@ -21,6 +23,21 @@ public class JWTAuthenticationFilter extends OncePerRequestFilter {
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        String token = resolveToken(request);
 
+        if (token != null && jwtProvider.validateToken(token)) {
+            Authentication auth = jwtProvider.getAuthentication(token); // 사용자 추출
+            SecurityContextHolder.getContext().setAuthentication(auth); // SecurityContextHolder에 사용자 등록
+        }
+
+        filterChain.doFilter(request, response);
+    }
+
+    private String resolveToken(HttpServletRequest request) { // 헤더에서 토큰 추출
+        String bearerToken = request.getHeader("Authorization"); // 헤더의 Authorization에서 값을 가져옴.
+        if (bearerToken != null && bearerToken.startsWith("Bearer ")) { // Bearer를 제외한 토큰 값 가져오기
+            return bearerToken.substring(7);
+        }
+        return null;
     }
 }
