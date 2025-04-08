65ca8c6397cf6849241b56f77094b810e62c2cbd
Etwashoeren <sungmin0609@nate.com>
2025-04-08 21:52:00 +0900

    #3 feat: Member Entity 작성

--- src/main/java/com/se/dandan/entity/Member.java
+++ src/main/java/com/se/dandan/entity/Member.java
@@ -1,4 +1,27 @@
 package com.se.dandan.entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.*;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class Member {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String nickname;
+
+    private String userId;
+
+    private String password;
+
 }
