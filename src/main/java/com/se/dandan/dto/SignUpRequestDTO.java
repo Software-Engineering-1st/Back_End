c9e18f8c26fa4add6a6c7a1979a6416e984c6298
Etwashoeren <sungmin0609@nate.com>
2025-04-08 21:58:39 +0900

    #3 feat: 회원가입 RequestDTO 작성

--- src/main/java/com/se/dandan/dto/SignUpRequestDTO.java
+++ src/main/java/com/se/dandan/dto/SignUpRequestDTO.java
@@ -1,4 +1,23 @@
 package com.se.dandan.dto;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import lombok.Data;
+
+@Data
 public class SignUpRequestDTO {
+
+    @NotBlank
+    private String nickname;
+
+    @NotBlank
+    @Pattern(regexp = "^(?=.*[a-zA-Z])(?=.*\\d)[a-zA-Z\\d]{7,15}$") // 영문자, 숫자를 포함한 7글자 이상 15글자 이하
+    private String userId;
+
+    @NotBlank
+    @Pattern(regexp = "^(?=.*[A-Za-z])(?=.*\\d)(?=.*[@#$%^&+=!]).{8,}$") // 영문자, 특수문자, 숫자를 포함한 최소 8자 이상의 문자
+    private String password;
+
+    private String checkPassword;
+
 }
