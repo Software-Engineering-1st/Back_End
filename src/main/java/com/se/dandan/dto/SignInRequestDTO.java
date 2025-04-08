f1c2d7530491e0a79ce3f31462ddb2836d1748c5
Etwashoeren <sungmin0609@nate.com>
2025-04-08 21:59:25 +0900

    #3 feat: 로그인 RequestDTO 작성

--- src/main/java/com/se/dandan/dto/SignInRequestDTO.java
+++ src/main/java/com/se/dandan/dto/SignInRequestDTO.java
@@ -1,4 +1,14 @@
 package com.se.dandan.dto;
 
+import jakarta.validation.constraints.NotBlank;
+import lombok.Data;
+
+@Data
 public class SignInRequestDTO {
+
+    @NotBlank
+    private String nickname;
+
+    @NotBlank
+    private String password;
 }
