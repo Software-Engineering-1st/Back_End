cfbbd715c47aeb3138ad13116d66698f7fdee3c7
Etwashoeren <sungmin0609@nate.com>
2025-04-08 22:53:57 +0900

    #3 feat: JWT DTO 작성

--- src/main/java/com/se/dandan/dto/TokenInfo.java
+++ src/main/java/com/se/dandan/dto/TokenInfo.java
@@ -1,4 +1,18 @@
 package com.se.dandan.dto;
 
+import lombok.AccessLevel;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class TokenInfo {
+
+    private String grantType;
+
+    private String accessToken;
+
+    private String refreshToken;
+
+    private String role;
 }
