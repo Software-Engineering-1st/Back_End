9ed51f331c7b6fa49daa4d27a12c808482439b11
Etwashoeren <sungmin0609@nate.com>
2025-04-29 23:35:42 +0900

    #3 feat: 회원가입 기능 구현

--- src/main/java/com/se/dandan/service/AuthService.java
+++ src/main/java/com/se/dandan/service/AuthService.java
@@ -3,6 +3,7 @@ package com.se.dandan.service;
 import com.se.dandan.dto.IdCheckRequestDTO;
 import com.se.dandan.dto.SignUpRequestDTO;
 import com.se.dandan.entity.Member;
+import com.se.dandan.entity.MemberRole;
 import com.se.dandan.exception.CustomException;
 import com.se.dandan.exception.ErrorCode;
 import com.se.dandan.repository.MemberRepository;
@@ -28,5 +29,38 @@ public class AuthService {
         if (existedUserId) {
             throw new CustomException(ErrorCode.DUPLICATED_ID);
         }
+    }
+
+    public void signUp(SignUpRequestDTO signUpRequestDTO) {
+        String nickname = signUpRequestDTO.getNickname();
+
+        String userId = signUpRequestDTO.getUserId();
+        boolean existedUserId = memberRepository.existsByUserId(userId);
+
+        if (existedUserId) {
+            throw new CustomException(ErrorCode.DUPLICATED_ID);
+        }
+
+        String password = signUpRequestDTO.getPassword();
+        String checkPassword = signUpRequestDTO.getCheckPassword();
+
+        if(!password.equals(checkPassword)) {
+            throw new CustomException(ErrorCode.NOT_MATCHED_PASSWORD);
+        }
+
+        String encodedPassword = bCryptPasswordEncoder.encode(password);
+        signUpRequestDTO.setPassword(encodedPassword);
+
+        int wordCount = signUpRequestDTO.getWordCount();
+
+        Member member = Member.builder()
+                .nickname(nickname)
+                .userId(userId)
+                .password(encodedPassword)
+                .wordCount(wordCount)
+                .role(MemberRole.MEMBER)
+                .build();
+
+        memberRepository.save(member);
     }
 }
