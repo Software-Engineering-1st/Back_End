d1b025b330752bfffe34fdfd67e69ecb79ba3a40
Etwashoeren <sungmin0609@nate.com>
2025-05-06 21:53:51 +0900

    #3 refactor: ID Check Pattern 어노테이션 메시지 추가

--- src/main/java/com/se/dandan/dto/IdCheckRequestDTO.java
+++ src/main/java/com/se/dandan/dto/IdCheckRequestDTO.java
@@ -8,7 +8,8 @@ import lombok.Data;
 public class IdCheckRequestDTO {
 
     @NotBlank
-    @Pattern(regexp = "^(?=.*[a-zA-Z])(?=.*\\d)[a-zA-Z\\d]{7,15}$") // 영문자, 숫자를 포함한 7글자 이상 15글자 이하
+    @Pattern(regexp = "^(?=.*[a-zA-Z])(?=.*\\d)[a-zA-Z\\d]{7,15}$",
+            message = "아이디는 영문자와 숫자를 포함하여 7자 이상 15자 이하로 입력해주세요.") // 영문자, 숫자를 포함한 7글자 이상 15글자 이하
     private String userId;
 
 }
