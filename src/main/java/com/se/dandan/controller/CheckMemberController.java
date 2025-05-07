ab165074c49051b9d04ef9347041d737f3a4eec0
Etwashoeren <sungmin0609@nate.com>
2025-05-08 01:32:38 +0900

    #5 feat: Swagger 어노테이션 추가

--- src/main/java/com/se/dandan/controller/CheckMemberController.java
+++ src/main/java/com/se/dandan/controller/CheckMemberController.java
@@ -2,15 +2,24 @@ package com.se.dandan.controller;
 
 import com.se.dandan.dto.MemberPrincipalDTO;
 import com.se.dandan.dto.common.ResponseTemplate;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Tag(name = "CheckMember API", description = "현재 사용자 정보 추출 API")
 @RestController
 public class CheckMemberController {
 
+    @Operation(summary = "사용자 확인 컨트롤러", description = "사용자 확인을 요청하는 컨트롤러 입니다.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "성공"),
+    })
     @GetMapping("/test")
     public ResponseTemplate<String> test(@AuthenticationPrincipal MemberPrincipalDTO member) {
 
--- src/main/java/com/se/dandan/dto/MemberPrincipalDTO.java
+++ src/main/java/com/se/dandan/dto/MemberPrincipalDTO.java
@@ -1,13 +1,17 @@
 package com.se.dandan.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Builder;
 import lombok.Data;
 
+@Schema(description = "로그인 시, 사용자 정보 저장 DTO")
 @Data
 @Builder
 public class MemberPrincipalDTO {
 
+    @Schema(description = "DB 상의 PK 값")
     private Long id;
 
+    @Schema(description = "사용자 닉네임")
     private String nickname;
 }
