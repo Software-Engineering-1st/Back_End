a5d742d6d99c7d685cd6e2c3c8e59e18edacab59
Etwashoeren <sungmin0609@nate.com>
2025-05-08 01:21:21 +0900

    #5 feat: JWT 로그인 시, 사용자 객체 DTO 작성

---
+++ src/main/java/com/se/dandan/dto/MemberPrincipalDTO.java
@@ -0,0 +1,13 @@
+package com.se.dandan.dto;
+
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
+public class MemberPrincipalDTO {
+
+    private Long id;
+
+    private String nickname;
+}
