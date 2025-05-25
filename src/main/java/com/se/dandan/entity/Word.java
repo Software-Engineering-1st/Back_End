d238954f09565cfa74ce33017888d1a038a598d2
Etwashoeren <sungmin0609@nate.com>
2025-05-26 01:21:15 +0900

    #10 feat: 단어 entity 생성

---
+++ src/main/java/com/se/dandan/entity/Word.java
@@ -0,0 +1,28 @@
+package com.se.dandan.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.*;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Word {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String english;
+
+    private String meaning1;
+
+    private String meaning2;
+
+    private String meaning3;
+}
