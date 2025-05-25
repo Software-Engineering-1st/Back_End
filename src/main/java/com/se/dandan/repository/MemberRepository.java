d66f532e0a1122eb8f3ba9947603a9fbe11a903a
Etwashoeren <sungmin0609@nate.com>
2025-05-26 01:24:37 +0900

    #10 feat: findById 메소드 추가

--- src/main/java/com/se/dandan/repository/MemberRepository.java
+++ src/main/java/com/se/dandan/repository/MemberRepository.java
@@ -1,14 +1,16 @@
 package com.se.dandan.repository;
 
 import com.se.dandan.entity.Member;
+import jakarta.validation.constraints.NotNull;
+import lombok.NonNull;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
 public interface MemberRepository extends JpaRepository<Member, Long> {
-    Member findByNickname(String nickname);
-
     Optional<Member> findByUserId(String userId);
 
     boolean existsByUserId(String userId);
+
+    Optional<Member> findById(@NonNull Long memberId);
 }
