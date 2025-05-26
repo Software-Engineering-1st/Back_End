06a6fba1753152633481890cb03a583507ce8ed4
Etwashoeren <sungmin0609@nate.com>
2025-05-27 02:21:33 +0900

    #13 feat: 토큰 블랙리스트 Repo 메소드 추가

--- src/main/java/com/se/dandan/repository/BlacklistedTokenRepository.java
+++ src/main/java/com/se/dandan/repository/BlacklistedTokenRepository.java
@@ -3,6 +3,9 @@ package com.se.dandan.repository;
 import com.se.dandan.entity.BlacklistedToken;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.time.LocalDateTime;
+
 public interface BlacklistedTokenRepository extends JpaRepository<BlacklistedToken, Long> {
-    boolean existsByRefreshToken(String refreshToken);
+    boolean existsByToken(String token);
+    void deleteAllByExpirationBefore(LocalDateTime time);
 }
