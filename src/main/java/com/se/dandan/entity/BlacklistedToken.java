5fd6e51f476ed9bf406895ab69e44db82d6fe5f8
Etwashoeren <sungmin0609@nate.com>
2025-05-27 00:52:22 +0900

    #13 feat: 토큰 블랙리스트 엔티티 생성

---
+++ src/main/java/com/se/dandan/entity/BlacklistedToken.java
@@ -0,0 +1,30 @@
+package com.se.dandan.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class BlacklistedToken {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String refreshToken;
+
+    private String userId;
+
+    private LocalDateTime expiration;
+}
