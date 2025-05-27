e3c7737d5c5d7046d703c5ba6f6632c29d034a74
Etwashoeren <sungmin0609@nate.com>
2025-05-27 17:08:08 +0900

    refactor: 프론트엔드 port 번호 변경

--- src/main/java/com/se/dandan/config/SecurityConfig.java
+++ src/main/java/com/se/dandan/config/SecurityConfig.java
@@ -78,7 +78,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("http://localhost:3000")); // 로컬 환경에서 CORS 허용
+        configuration.setAllowedOrigins(List.of("http://localhost:5173")); // 로컬 환경에서 CORS 허용
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE"));
         configuration.setAllowedHeaders(List.of("*"));
         configuration.setAllowCredentials(true);
